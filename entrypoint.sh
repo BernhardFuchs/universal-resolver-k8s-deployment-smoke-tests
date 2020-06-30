f04299809e29fff361aa446425da185c1fcc3c1f
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-06-30 14:27:24 +0200

    feat: create result json file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ sh -c "echo $*"
 
 echo "$INPUT_HOST"
 echo "$INPUT_CONFIG"
+echo "$INPUT_OUT_FOLDER"
 
 echo "Current folder"
 pwd
@@ -22,4 +23,6 @@ cat /github/workspace/deploy/uni-resolver-ingress.yaml
 
 python --version
 
-python /smoke-tests/smoke-test.py --host "$INPUT_HOST" --config "$INPUT_CONFIG"
+python /smoke-tests/smoke-test.py --host "$INPUT_HOST" --config "$INPUT_CONFIG" --out "$INPUT_OUT_FOLDER"
+
+ls -al /smoke-tests
